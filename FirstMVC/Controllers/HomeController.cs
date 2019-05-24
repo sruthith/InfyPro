427283e413a3eab42ea53092a249751941078038
Sruthith Chelimela <SChelimela_GPS@nec.edu>
2019-05-24 03:32:50 -0400

    initial commit

---
+++ FirstMVC/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Office()
+        {
+            return View();
+        }
+        public ActionResult Transportation()
+        {
+            return View();
+        }
+        public ActionResult ParkingLot()
+        {
+            return View();
+        }
+        public ActionResult ApartmentsAndBuildings()
+        {
+            return View();
+        }
+        public ActionResult Shopping()
+        {
+            return View();
+        }
+        public ActionResult Restaurants()
+        {
+            return View();
+        }
+    }
+}
